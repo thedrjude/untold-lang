00583cfaa3269c677e912fd0511fe25892423ee9
Antony Jude <its.drjude@proton.me>
2026-04-22 18:51:54 +0530

    Bump version to 2.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="untold-lang",
-    version="2.2.5",
+    version="2.2.6",
     description="Untold Lang v2.2.0 — The Language Without Limits",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
